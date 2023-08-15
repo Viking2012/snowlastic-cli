d96b877681db67dd8463e7468627d0d3bed7ca17
AJO <viking2012@gmail.com>
2023-08-15 10:10:27 -0400

    fix unhandled error in fmt.Fprintln

--- cmd/root.go
+++ cmd/root.go
@@ -140,7 +140,7 @@ func initConfig() {
 
 	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
-		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+		_, _ = fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
 	}
 }
 
