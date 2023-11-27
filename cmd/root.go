248887eff908ffdfec49e775c75526cc58bc86c9
Alexander Orban <Alexander.Orban@gmail.com>
2023-11-27 13:15:56 -0500

    bump version

--- cmd/root.go
+++ cmd/root.go
@@ -42,7 +42,7 @@ var (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:     "snowlastic-cli",
-	Version: "2.0.0",
+	Version: "2.1.0",
 	Short:   "Manage, update, and administer an elasticsearch server",
 	Long: `Interact with an elasticsearch server, including indexing documents
 from a snowflake database. For example:
